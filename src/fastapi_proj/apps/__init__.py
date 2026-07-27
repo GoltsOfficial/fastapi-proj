f4b20429a245b5e87e366359dae3b56019bca76a
Pavel Chernous <goltshteyngottorp@gmail.com>
2026-07-28 02:50:14 +0300

    feat: realizelogout for user and profile app

--- src/fastapi_proj/apps/__init__.py
+++ src/fastapi_proj/apps/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from fastapi_proj.apps.auth.routes import auth_router
+from fastapi_proj.apps.profile.routes import profile_router
 
 apps_router = APIRouter(prefix="/api/v1")
 
 apps_router.include_router(router=auth_router)
+apps_router.include_router(router=profile_router)
--- src/fastapi_proj/apps/auth/services.py
+++ src/fastapi_proj/apps/auth/services.py
@@ -5,7 +5,12 @@ from starlette.responses import JSONResponse
 
 from fastapi_proj.apps.auth.handlers import AuthHandler
 from fastapi_proj.apps.auth.managers import UserManager
-from fastapi_proj.apps.auth.schemas import AuthUser, CreateUser, UserReturnData
+from fastapi_proj.apps.auth.schemas import (
+    AuthUser,
+    CreateUser,
+    UserReturnData,
+    UserVerifySchema,
+)
 from fastapi_proj.apps.auth.tasks import send_confirmation_email
 from fastapi_proj.core.settings import settings
 
@@ -79,7 +84,7 @@ class UserService:
         """
         exist_user = await self.manager.get_user_by_email(email=user.email)
 
-        if exist_user is None or not self.handler.verify_password(
+        if exist_user is None or not await self.handler.verify_password(
             hashed_password=exist_user.hashed_password, raw_password=user.password
         ):
             raise HTTPException(
@@ -98,5 +103,22 @@ class UserService:
             httponly=True,
             max_age=settings.access_token_expire,
         )
+
+        return response
+
+    async def logout_user(self, user: UserVerifySchema) -> JSONResponse:
+        """
+        Отправляет запрос на выход пользователя из системы.
+
+        :param user: Схема, содержащая информацию о пользователе для аутентификации.
+        :type user: UserVerifySchema
+        :returns: Ответ сервера с сообщением об успешном выходе пользователя.
+        :rtype: JSONResponse
+        :raises Exception: Если произошла ошибка при отмене токена доступа.
+        """
+        await self.manager.revoke_access_token(user_id=user.id, session_id=user.session_id)
+
+        response = JSONResponse(content={"message": "Logged out"})
+        response.delete_cookie(key="Authorization")
 
         return response
